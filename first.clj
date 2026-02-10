40398df2eddb27a32c3af81de1b714ae831e3167
cauen <emanuel-caue@hotmail.com>
2026-02-10 19:57:58 -0300

    Add deps.edn for dependency management and refactor first.clj to include a main function that sums two numbers.

--- first.clj
+++ first.clj
@@ -1,6 +1,10 @@
-;; This is a comment
+(ns first)
 
-(defn add [a b]
-  (+ a b))
+(defn add [left-number right-number]
+  (+ left-number right-number))
 
-(println (add 1 2))
+(defn -main [& _args]
+  (let [first-number 1
+        second-number 2
+        sum-result (add first-number second-number)]
+    (println sum-result)))
